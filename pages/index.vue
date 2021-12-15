64527552f43ed49e2404c1252fbfc39906b3968d
denfin <kontakt@dennisfink.de>
2021-12-15 15:45:53 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <figure>
+      <img src="/untype_dennis.png" alt="">
+    </figure>
+    <strong>Dennis Fink</strong><br>
+    <span>Webentwicklung</span><br><br>
+    <a href="mailto:dennis@untype.agency">dennis@untype.agency</a><br>
+    <a href="tel:+491606091990">0160 609 1990</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+body, figure{
+  margin: 0;
+}
+img {
+  width: 200px
+}
+
+</style>
